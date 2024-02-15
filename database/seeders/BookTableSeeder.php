5c1f363a37584f512be5b80863102bf701d00a22
ChrisGarozzo <garozzochristian@gmail.com>
2024-02-15 16:37:01 +0100

    added scaffolding models&Factory&Seeder

--- database/seeders/BookTableSeeder.php
+++ database/seeders/BookTableSeeder.php
@@ -6,15 +6,15 @@ use App\Models\Book;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class BookTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this-> call([BookTableSeeder :: class]);
+        Book :: factory() -> count(30) -> create();
     }
 }
